674f621bf76fa9d5017c1acdbdb5207c14b1239a
knn <nikolai.kuznetsov.nk@gmail.com>
2022-04-29 09:55:40 +0700

    Добавлена визуальная составляющая проекта

--- UserAuthorization/MainWindow.xaml.cs
+++ UserAuthorization/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace UserAuthorization
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
